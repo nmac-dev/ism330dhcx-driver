3ef955ebb15f4f50a27f32643cfc0bc3b6303607
nmac-dev <ntwmac.dev@gmail.com>
2025-02-22 13:15:09 +0000

    :ADDED: internal SH structs for sensor hub output registers

--- driver/internal/rm_sh_structs.h
+++ driver/internal/rm_sh_structs.h
@@ -11,4 +11,97 @@
 /// NOTE: Sensor Hub registers are accessible when bit SHUB_REG_ACCESS is set to '1' in FUNC_CFG_ACCESS
 
 
+/* Sensor Hub Output Registers */
+
+
+/// @struct _ism330dhcx_rm_sh__SENSOR_HUB_1_s
+/// @brief  ISM330DHCX Sensor hub output 1 register [r] {output}
+typedef reg_u8_s _ism330dhcx_rm_sh__SENSOR_HUB_1_s;
+
+
+/// @struct _ism330dhcx_rm_sh__SENSOR_HUB_2_s
+/// @brief  ISM330DHCX Sensor hub output 2 register [r] {output}
+typedef reg_u8_s _ism330dhcx_rm_sh__SENSOR_HUB_2_s;
+
+
+/// @struct _ism330dhcx_rm_sh__SENSOR_HUB_3_s
+/// @brief  ISM330DHCX Sensor hub output 3 register [r] {output}
+typedef reg_u8_s _ism330dhcx_rm_sh__SENSOR_HUB_3_s;
+
+
+/// @struct _ism330dhcx_rm_sh__SENSOR_HUB_4_s
+/// @brief  ISM330DHCX Sensor hub output 4 register [r] {output}
+typedef reg_u8_s _ism330dhcx_rm_sh__SENSOR_HUB_4_s;
+
+
+/// @struct _ism330dhcx_rm_sh__SENSOR_HUB_5_s
+/// @brief  ISM330DHCX Sensor hub output 5 register [r] {output}
+typedef reg_u8_s _ism330dhcx_rm_sh__SENSOR_HUB_5_s;
+
+
+/// @struct _ism330dhcx_rm_sh__SENSOR_HUB_6_s
+/// @brief  ISM330DHCX Sensor hub output 6 register [r] {output}
+typedef reg_u8_s _ism330dhcx_rm_sh__SENSOR_HUB_6_s;
+
+
+/// @struct _ism330dhcx_rm_sh__SENSOR_HUB_7_s
+/// @brief  ISM330DHCX Sensor hub output 7 register [r] {output}
+typedef reg_u8_s _ism330dhcx_rm_sh__SENSOR_HUB_7_s;
+
+
+/// @struct _ism330dhcx_rm_sh__SENSOR_HUB_8_s
+/// @brief  ISM330DHCX Sensor hub output 8 register [r] {output}
+typedef reg_u8_s _ism330dhcx_rm_sh__SENSOR_HUB_8_s;
+
+
+/// @struct _ism330dhcx_rm_sh__SENSOR_HUB_9_s
+/// @brief  ISM330DHCX Sensor hub output 9 register [r] {output}
+typedef reg_u8_s _ism330dhcx_rm_sh__SENSOR_HUB_9_s;
+
+
+/// @struct _ism330dhcx_rm_sh__SENSOR_HUB_10_s
+/// @brief  ISM330DHCX Sensor hub output 10 register [r] {output}
+typedef reg_u8_s _ism330dhcx_rm_sh__SENSOR_HUB_10_s;
+
+
+/// @struct _ism330dhcx_rm_sh__SENSOR_HUB_11_s
+/// @brief  ISM330DHCX Sensor hub output 11 register [r] {output}
+typedef reg_u8_s _ism330dhcx_rm_sh__SENSOR_HUB_11_s;
+
+
+/// @struct _ism330dhcx_rm_sh__SENSOR_HUB_12_s
+/// @brief  ISM330DHCX Sensor hub output 12 register [r] {output}
+typedef reg_u8_s _ism330dhcx_rm_sh__SENSOR_HUB_12_s;
+
+
+/// @struct _ism330dhcx_rm_sh__SENSOR_HUB_13_s
+/// @brief  ISM330DHCX Sensor hub output 13 register [r] {output}
+typedef reg_u8_s _ism330dhcx_rm_sh__SENSOR_HUB_13_s;
+
+
+/// @struct _ism330dhcx_rm_sh__SENSOR_HUB_14_s
+/// @brief  ISM330DHCX Sensor hub output 14 register [r] {output}
+typedef reg_u8_s _ism330dhcx_rm_sh__SENSOR_HUB_14_s;
+
+
+/// @struct _ism330dhcx_rm_sh__SENSOR_HUB_15_s
+/// @brief  ISM330DHCX Sensor hub output 15 register [r] {output}
+typedef reg_u8_s _ism330dhcx_rm_sh__SENSOR_HUB_15_s;
+
+
+/// @struct _ism330dhcx_rm_sh__SENSOR_HUB_16_s
+/// @brief  ISM330DHCX Sensor hub output 16 register [r] {output}
+typedef reg_u8_s _ism330dhcx_rm_sh__SENSOR_HUB_16_s;
+
+
+/// @struct _ism330dhcx_rm_sh__SENSOR_HUB_17_s
+/// @brief  ISM330DHCX Sensor hub output 17 register [r] {output}
+typedef reg_u8_s _ism330dhcx_rm_sh__SENSOR_HUB_17_s;
+
+
+/// @struct _ism330dhcx_rm_sh__SENSOR_HUB_18_s
+/// @brief  ISM330DHCX Sensor hub output 18 register [r] {output}
+typedef reg_u8_s _ism330dhcx_rm_sh__SENSOR_HUB_18_s;
+
+
 #endif /* __ISM330DHCX_INTERNAL__RM_SH_STRUCTS_H__ */
