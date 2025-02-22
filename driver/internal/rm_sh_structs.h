d35cc085bf99e6dd404c367e1e72467b0c115771
nmac-dev <ntwmac.dev@gmail.com>
2025-02-22 12:29:00 +0000

    :ADDED: internal/rm_sh_structs.h

---
+++ driver/internal/rm_sh_structs.h
@@ -0,0 +1,14 @@
+/// @file  internal/rm_sh_structs.h
+/// @brief ISM330DHCX Internal Structs for Sensor Hub Register Mapping
+
+#ifndef __ISM330DHCX_INTERNAL__RM_SH_STRUCTS_H__
+#define __ISM330DHCX_INTERNAL__RM_SH_STRUCTS_H__
+
+
+#include "c-nstd/types.h"
+
+
+/// NOTE: Sensor Hub registers are accessible when bit SHUB_REG_ACCESS is set to '1' in FUNC_CFG_ACCESS
+
+
+#endif /* __ISM330DHCX_INTERNAL__RM_SH_STRUCTS_H__ */
