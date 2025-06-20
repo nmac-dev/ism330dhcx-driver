d58153d5972cc0af5abcbe23d636f3046027a5b7
nmac-dev <ntwmac.dev@gmail.com>
2025-06-20 09:06:06 +0100

    feature | struct > MLC

---
+++ driver/mlc.h
@@ -0,0 +1,48 @@
+/// ISM330DHCX MLC Driver
+
+#ifndef __ISM330DHCX_MLC_H__
+#define __ISM330DHCX_MLC_H__
+
+
+#include "c-nstd/types.h"
+#include "ism330dhcx/register_map.h"
+#include "ism330dhcx/internal/rm_structs.h"
+#include "ism330dhcx/internal/rm_ef_structs.h"
+#include "ism330dhcx/internal/rm_eaf1_structs.h"
+
+
+/// @struct ism330dhcx_mlc_s
+/// @brief  ISM330DHCX MLC Struct
+typedef struct ism330dhcx_mlc_s
+{
+    /* Specs */
+    /* Control Registers */
+
+    _ism330dhcx_rm__MLC_STATUS_MAINPAGE_t mlc_status_mainpage;
+
+    _ism330dhcx_rm_ef__MLC_INT1_t mlc_int1;
+    _ism330dhcx_rm_ef__MLC_INT2_t mlc_int2;
+
+    _ism330dhcx_rm_ef__MLC_STATUS_t ef__mlc_status;
+
+    _ism330dhcx_rm_ef__MLC0_SRC_t ef__mlc0_src;
+    _ism330dhcx_rm_ef__MLC1_SRC_t ef__mlc1_src;
+    _ism330dhcx_rm_ef__MLC2_SRC_t ef__mlc2_src;
+    _ism330dhcx_rm_ef__MLC3_SRC_t ef__mlc3_src;
+    _ism330dhcx_rm_ef__MLC4_SRC_t ef__mlc4_src;
+    _ism330dhcx_rm_ef__MLC5_SRC_t ef__mlc5_src;
+    _ism330dhcx_rm_ef__MLC6_SRC_t ef__mlc6_src;
+    _ism330dhcx_rm_ef__MLC7_SRC_t ef__mlc7_src;
+
+    _ism330dhcx_rm_eaf1__MLC_MAG_SENSITIVITY_t eaf1__mlc_mag_sensitivity;
+
+    /* Configuration */
+
+} ism330dhcx_mlc_t;
+
+
+/// @brief  Fetches the default struct configuration for the ism330dhcx MLC struct
+/// @return default ism330dhcx MLC struct
+ism330dhcx_mlc_s ism330dhcx_mlc__get_default_s();
+
+#endif /* __ISM330DHCX_MLC_H__ */
