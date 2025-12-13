47c57075d18cf6247e805f0301cbecde7586b1ce
nmac-dev <ntwmac.dev@gmail.com>
2025-12-13 21:51:38 +0000

    compile-test | fixed incorrect build

--- test/compile/test-compile.sh
+++ test/compile/test-compile.sh
@@ -1,5 +1,8 @@
 echo "-- Test Compile --"
 
+
+## Variables
+
 REPO__D="$(git rev-parse --show-toplevel)"
 DRIVER__D="$REPO__D/driver"
 SRC__D="$DRIVER__D/src"
@@ -13,26 +16,37 @@ ISM330DHCX__LIB="ism330dhcx"
 ln -sf "$REPO__D/driver/$C_NSTD__LIB" "$C_NSTD__LIB"
 ln -sf "$REPO__D/driver"              "$ISM330DHCX__LIB"
 
-SRC_C=("$SRC__D/core.c")
+
+## Sources
+
+# Compile all driver source files so new modules are picked up automatically
+SRC__C=( "$SRC__D"/*.c )
 
 
 ## Main
 
-TEST_MAIN__DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-MAIN_C="test_compile.c"
-OUT="test_out"
+TEST_MAIN__D="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+MAIN__C="$TEST_MAIN__D/test_compile.c"
+OUT="$TEST_MAIN__D/test_out"
 
 
 ## Compile Test
 
-gcc $SRC_C              \
-    $MAIN_C             \
-    -I $TEST_MAIN__DIR  \
+gcc    "${SRC__C[@]}" \
+    $MAIN__C      \
+    -I $TEST_MAIN__D \
     -o $OUT
+
+
+if [[ $? -ne 0 ]]; then
+    echo "FAILED"
+    exit 1
+fi
+
 
 chmod +x $OUT
 
-RESULT="$(./$OUT)"
+RESULT="$($OUT)"
 
 if [[ $RESULT =~ "Hello, World!\n" ]]; then
     echo "FAILED"
--- test/compile/test_compile.c
+++ test/compile/test_compile.c
@@ -18,22 +18,22 @@
 #include "ism330dhcx/sensor_hub.h"
 
 
-/// @brief  main
+/// @brief main
 /// @param argc
 /// @param argv
 /// @return non-zero on failure
 int main(int argc, char const *argv[])
 {
-    ism330dhcx_acc_s        acc        = ism330dhcx_acc__get_default_s();
-    ism330dhcx_core_s       core       = ism330dhcx_core__get_default_s();
-    ism330dhcx_core_s       emag       = ism330dhcx_emag__get_default_s();
-    ism330dhcx_fifo_s       fifo       = ism330dhcx_fifo__get_default_s();
-    ism330dhcx_core_s       fsm        = ism330dhcx_fsm__get_default_s();
-    ism330dhcx_gyr_s        gyr        = ism330dhcx_gyr__get_default_s();
-    ism330dhcx_mlc_s        mlc        = ism330dhcx_mlc__get_default_s();
-    ism330dhcx_motion_s     motion     = ism330dhcx_motion__get_default_s();
-    ism330dhcx_ois_s        ois        = ism330dhcx_ois__get_default_s();
-    ism330dhcx_sensor_hub_s sensor_hub = ism330dhcx_sensor_hub__get_default_s();
+    ism330dhcx_acc_s    acc    = ism330dhcx_acc__get_default_s();
+    ism330dhcx_core_s   core   = ism330dhcx_core__get_default_s();
+    ism330dhcx_emag_s   emag   = ism330dhcx_emag__get_default_s();
+    ism330dhcx_fifo_s   fifo   = ism330dhcx_fifo__get_default_s();
+    ism330dhcx_fsm_s    fsm    = ism330dhcx_fsm__get_default_s();
+    ism330dhcx_gyr_s    gyr    = ism330dhcx_gyr__get_default_s();
+    ism330dhcx_mlc_s    mlc    = ism330dhcx_mlc__get_default_s();
+    ism330dhcx_motion_s motion = ism330dhcx_motion__get_default_s();
+    ism330dhcx_ois_s    ois    = ism330dhcx_ois__get_default_s();
+    ism330dhcx_sh_s     sh     = ism330dhcx_sh__get_default_s();
 
     printf("Hello, World!" "\n");
     return 0;
