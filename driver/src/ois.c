1cb3eeb2da26016e3d34acf2ea7f243ff6784b2c
nmac-dev <ntwmac.dev@gmail.com>
2025-12-13 21:14:11 +0000

    ois | added default struct function

---
+++ driver/src/ois.c
@@ -0,0 +1,33 @@
+/// @file  ois.c
+/// @brief ISM330DHCX Optical Image Stabilisation Driver
+
+#include "ism330dhcx/ois.h"
+
+
+ism330dhcx_ois_s ism330dhcx_ois__get_default_s()
+{
+    return (ism330dhcx_ois_s){
+
+        /* Device Registers */
+
+        .int_st = {
+            .ADR = ISM330DHCX_RM__INT_OIS,
+            .bf  = 0x00u,
+        },
+
+        .ctrl1 = {
+            .ADR = ISM330DHCX_RM__CTRL1_OIS,
+            .bf  = 0x00u,
+        },
+
+        .ctrl2 = {
+            .ADR = ISM330DHCX_RM__CTRL2_OIS,
+            .bf  = 0x00u,
+        },
+
+        .ctrl3 = {
+            .ADR = ISM330DHCX_RM__CTRL3_OIS,
+            .bf  = 0x00u,
+        },
+    };
+}
