104da9515ef9dc117363b5d1a78b1532e2339a90
nmac-dev <ntwmac.dev@gmail.com>
2026-05-27 01:02:41 +0100

    types.h | reworked types for better readability and consistency

--- README.md
+++ README.md
@@ -26,14 +26,15 @@ ISM330DHCX Driver
 
 ### Types
 
+
 Defines the types in `types.h`, which follow the convention: `[<prefix>_]<base><size>[<qualifier>]_<construct>`
 
 i.e. `u32c_t` would be `unsigned int const` with the `_t` denoting the entity is a primitive data type
 
 **Segment**     | Description                 | Example
- :------------: | :-------------------------- | :----------------------------------------------
+:------------: | :-------------------------- | :----------------------------------------------
 `[<prefix>]`    | simply put, the prefix name | `reg_` register, `adr_` address
-`<base>`        | base (core) data type       | `u` unsigned, `s` signed, `f` float
+`<base>`        | base (core) data type       | `u` unsigned integer, `i` signed integer, `f` float, `c` character, `bool` boolean
 `<size>`        | size of the type in bits    | `8` bits, `16` bits, `32` bits, `64`bits
 `[<qualifier>]` | optional variable qualifier | `c` const, `v` volatile
 `_<construct>`  | programming construct       | `_t` primitive type, `_s` struct, `_u` union, `_e` enum
@@ -41,57 +42,71 @@ i.e. `u32c_t` would be `unsigned int const` with the `_t` denoting the entity is
 **Exceptions**:
  - `void *` is denoted as `v0_t`, since the size cannot be defined until compilation
 
-
 <details>
 <summary>Types List (expand ...)</summary>
-
 
 The following types are defined in `ism330dhcx/types.h`.
 
 **Primitive type aliases**
 
-| Type     | Description
-| :------- | :-------
-| `u8_t`   | unsigned 8-bit integer
-| `u8c_t`  | `u8_t const`
-| `s8_t`   | signed 8-bit integer
-| `s8c_t`  | `s8_t const`
-| `u16_t`  | unsigned 16-bit integer
-| `u16c_t` | `u16_t const`
-| `s16_t`  | signed 16-bit integer
-| `s16c_t` | `s16_t const`
-| `u32_t`  | unsigned 32-bit integer
-| `u32c_t` | `u32_t const`
-| `s32_t`  | signed 32-bit integer
-| `s32c_t` | `s32_t const`
-| `u64_t`  | unsigned 64-bit integer
-| `u64c_t` | `u64_t const`
-| `s64_t`  | signed 64-bit integer
-| `s64c_t` | `s64_t const`
-| `f32_t`  | 32-bit floating point
-| `f32c_t` | `f32_t const`
-| `f64_t`  | 64-bit floating point
-| `f64c_t` | `f64_t const`
-| `v0_t`   | `void` absence of value
-| `v0c_t`  | `v0_t` `const`
+| Type        | Description
+| :---------- | :-----------------------------
+| `bool8_t`   | 8-bit boolean (storage)
+| `bool_t`    | native boolean (logic, usually int)
+| `i8_t`      | signed 8-bit integer
+| `i8c_t`     | `i8_t const`
+| `u8_t`      | unsigned 8-bit integer
+| `u8c_t`     | `u8_t const`
+| `c8_t`      | 8-bit character
+| `c8c_t`     | `c8_t const`
+| `c8p_t`     | pointer to 8-bit character
+| `c8r_t`     | pointer to const 8-bit character
+| `i16_t`     | signed 16-bit integer
+| `i16c_t`    | `i16_t const`
+| `u16_t`     | unsigned 16-bit integer
+| `u16c_t`    | `u16_t const`
+| `c16_t`     | 16-bit character
+| `c16c_t`    | `c16_t const`
+| `c16p_t`    | pointer to 16-bit character
+| `c16r_t`    | pointer to const 16-bit character
+| `i32_t`     | signed 32-bit integer
+| `i32c_t`    | `i32_t const`
+| `u32_t`     | unsigned 32-bit integer
+| `u32c_t`    | `u32_t const`
+| `c32_t`     | 32-bit character
+| `c32c_t`    | `c32_t const`
+| `c32p_t`    | pointer to 32-bit character
+| `c32r_t`    | pointer to const 32-bit character
+| `i64_t`     | signed 64-bit integer
+| `i64c_t`    | `i64_t const`
+| `u64_t`     | unsigned 64-bit integer
+| `u64c_t`    | `u64_t const`
+| `f32_t`     | 32-bit floating point
+| `f32c_t`    | `f32_t const`
+| `f64_t`     | 64-bit floating point
+| `f64c_t`    | `f64_t const`
+| `v0_t`      | `void` absence of value
+| `v0c_t`     | `v0_t const`
+| `v0p_t`     | pointer to void
+| `v0r_t`     | pointer to const void
 
 **Address unions**
 
 | Type         | Description
-| :----------- | :-----------
-| `adr_u8_u`   | 8-bit address union with `byte_0`
+| :----------- | :--------------------------------------
+| `adr_u8_u`   | 8-bit address union with `byte._0`
 | `adr_u8c_u`  | `adr_u8_u const`
-| `adr_u16_u`  | 16-bit address union with `byte_0` and `byte_1`
+| `adr_u16_u`  | 16-bit address union with `byte._0`, `byte._1`
 | `adr_u16c_u` | `adr_u16_u const`
-| `adr_u32_u`  | 32-bit address union with `byte_0`..`byte_3`
+| `adr_u32_u`  | 32-bit address union with `byte._0`..`byte._3`
 | `adr_u32c_u` | `adr_u32_u const`
-| `adr_u64_u`  | 64-bit address union with `byte_0`..`byte_7`
+| `adr_u64_u`  | 64-bit address union with `byte._0`..`byte._7`
 | `adr_u64c_u` | `adr_u64_u const`
 
 **Register structs**
 
 | Type        | Description
-| :---------- | :----------
+| :---------- | :--------------------------------------
 | `reg_u8_s`  | 8-bit register struct with address and value
 | `reg_u16_s` | 16-bit register struct with address and value
 | `reg_u32_s` | 32-bit register struct with address and value
@@ -99,14 +114,14 @@ The following types are defined in `ism330dhcx/types.h`.
 
 **3D axis structs**
 
-| Type          | Description
-| :------------ | :------------
-| `axe3d_s8_s`  | 3-axis signed 8-bit structure
-| `axe3d_s16_s` | 3-axis signed 16-bit structure
-| `axe3d_s32_s` | 3-axis signed 32-bit structure
-| `axe3d_s64_s` | 3-axis signed 64-bit structure
-| `axe3d_f32_s` | 3-axis 32-bit float structure
-| `axe3d_f64_s` | 3-axis 64-bit float structure
+| Type           | Description
+| :------------- | :--------------------------------------
+| `axe3d_i8_s`   | 3-axis signed 8-bit structure
+| `axe3d_i16_s`  | 3-axis signed 16-bit structure
+| `axe3d_i32_s`  | 3-axis signed 32-bit structure
+| `axe3d_i64_s`  | 3-axis signed 64-bit structure
+| `axe3d_f32_s`  | 3-axis 32-bit float structure
+| `axe3d_f64_s`  | 3-axis 64-bit float structure
 
 </details>
 
--- ism330dhcx/types.h
+++ ism330dhcx/types.h
@@ -1,6 +1,6 @@
 /// @file  types.h
 /// @brief Defines type definitions [primitives, structures, unions, enums, pointers]
-/// @note  These types follow the convention: `[<prefix>_]<base><size>[<qualifier>]_<construct>`
+/// @note  All types follow the convention: [<prefix>_]<base><size>[<qualifier>]_<construct>
 
 #ifndef __ISM330DHCX_TYPES_H__
 #define __ISM330DHCX_TYPES_H__
@@ -11,276 +11,308 @@
 /// NOTE: __SIZEOF_<type>__ is defined by the compiler (typically gcc) but is not a guaranteed standard on all compilers
 
 
-/* (8-bit) char */
+/* 8-bit Types                                                      */
+/* ---------------------------------------------------------------- */
 
 
-typedef unsigned char u8_t;
-typedef u8_t    const u8c_t;
-#define U8_T__MAX ((u8_t)0xFF)              //  255
+typedef unsigned char bool8_t;              // 8-bit boolean type
 
-typedef signed char s8_t;
-typedef s8_t  const s8c_t;
-#define S8_T__MAX ((s8_t)0x7F)              //  127
-#define S8_T__MIN ((s8_t)(-S8_T__MAX - 1))  // -128
+typedef signed char i8_t;                   // 8-bit signed integer
+typedef i8_t  const i8c_t;                  // 8-bit signed integer constant
+#define I8_T__MAX ((i8_t)0x7F)              // 8-bit signed integer maximum value:  127
+#define I8_T__MIN ((i8_t)(-I8_T__MAX - 1))  // 8-bit signed integer minimum value: -128
 
-typedef char         str8_t;
-typedef str8_t const str8c_t;
-typedef str8c_t    * str8l_t;
+typedef unsigned char u8_t;                 // 8-bit unsigned integer
+typedef u8_t    const u8c_t;                // 8-bit unsigned integer constant
+#define U8_T__MAX ((u8_t)0xFF)              // 8-bit unsigned integer maximum value: 255
+
+
+#ifndef char8_t
+typedef char char8_t;            // 8-bit character
+#endif
+typedef char8_t const char8c_t;  // 8-bit character constant
+typedef char8_t     * str8p_t;   // 8-bit character string pointer
+typedef char8c_t    * str8r_t;   // 8-bit character string read-only pointer
+
 
 /// @union adr_u8_u
-/// @brief Address 8-bit ( byte_0 )
+/// @brief 8-bit address ( byte_0 )
 typedef union
 {
-    u8_t full;          // 8-bit register full address
+    u8_t full;    // 8-bit register full address
     struct {
-        u8_t byte_0;    // 8-bit register address byte 0
-    };
+        u8_t _0;  // 8-bit register address byte 0
+    } byte;       // 8-bit register address bytes
 } adr_u8_u;
 
+
 /// @union adr_u8c_u
-/// @brief Address 8-bit ( byte_0 )
+/// @brief 8-bit address constant ( byte_0 )
 typedef adr_u8_u const adr_u8c_u;
 
+
 /// @struct reg_u8_s
-/// @brief  Register 8-bit
+/// @brief  8-bit register
 typedef struct
 {
-    adr_u8c_u ADR;    // 8-bit register address
-    u8_t      val;    // 8-bit register value
+    adr_u8c_u ADR;  // 8-bit register address
+    u8_t      val;  // 8-bit register value
 } reg_u8_s;
 
-/// @struct axe3d_s8_s
-/// @brief  3D Axe 8-bit
+
+/// @struct axe3d_i8_s
+/// @brief  8-bit integer 3D axe
 typedef struct
 {
-    s8_t x;     // 8-bit x-axis
-    s8_t y;     // 8-bit y-axis
-    s8_t z;     // 8-bit z-axis
-} axe3d_s8_s;
+    i8_t x;  // 8-bit integer x-axis
+    i8_t y;  // 8-bit integer y-axis
+    i8_t z;  // 8-bit integer z-axis
+} axe3d_i8_s;
 
 
-/* (16-bit) short */
+/* 16-bit Types                                                     */
+/* ---------------------------------------------------------------- */
 
 
 #if defined(__SIZEOF_SHORT__) && VALIDATE_TYPE(__SIZEOF_SHORT__, 2)
 
-    typedef unsigned short u16_t;
-    typedef u16_t    const u16c_t;
-    #define U16_T__MAX ((u16_t)0xFFFF)              //  65535
+    typedef signed short i16_t;                    // 16-bit signed integer
+    typedef i16_t  const i16c_t;                   // 16-bit signed integer constant
+    #define I16_T__MAX ((i16_t)0x7FFF)             // 16-bit signed integer maximum value:  32767
+    #define I16_T__MIN ((i16_t)(-I16_T__MAX - 1))  // 16-bit signed integer minimum value: -32768
 
-    typedef signed short s16_t;
-    typedef s16_t  const s16c_t;
-    #define S16_T__MAX ((s16_t)0x7FFF)              //  32767
-    #define S16_T__MIN ((s16_t)(-S16_T__MAX - 1))   // -32768
+    typedef unsigned short u16_t;                  // 16-bit unsigned integer
+    typedef u16_t    const u16c_t;                 // 16-bit unsigned integer constant
+    #define U16_T__MAX ((u16_t)0xFFFF)             // 16-bit unsigned integer maximum value: 65535
 
-    typedef u16_t         str16_t;
-    typedef str16_t const str16c_t;
-    typedef str16c_t    * str16l_t;
+
+    #ifndef char16_t
+    typedef u16_t char16_t;            // 16-bit character
+    #endif
+    typedef u16_t          char16_t;
+    typedef char16_t const char16c_t;  // 16-bit character constant
+    typedef char16_t     * str16p_t;   // 16-bit character string pointer
+    typedef char16c_t    * str16r_t;   // 16-bit character string read-only pointer
+
 
     /// @union adr_u16_u
-    /// @brief Address 16-bit ( byte_0 | byte_1 )
+    /// @brief 16-bit address ( byte_0 | byte_1 )
     typedef union
     {
-        u16_t full;         // 16-bit register full address
+        u16_t full;   // 16-bit register full address
         struct {
-            u8_t byte_0;    // 16-bit register address byte 0
-            u8_t byte_1;    // 16-bit register address byte 1
-        };
+            u8_t _0;  // 16-bit register address byte 0
+            u8_t _1;  // 16-bit register address byte 1
+        } byte;       // 16-bit register address bytes
     } adr_u16_u;
 
+
     /// @union adr_u16c_u
-    /// @brief Address 16-bit ( byte_0 | byte_1 )
+    /// @brief 16-bit address ( byte_0 | byte_1 )
     typedef adr_u16_u const adr_u16c_u;
 
+
     /// @struct reg_u16_s
-    /// @brief  Register 16-bit
+    /// @brief  16-bit register
     typedef struct
     {
-        adr_u16c_u ADR;   // 16-bit register address
-        u16_t      val;   // 16-bit register value
+        adr_u16c_u ADR;  // 16-bit register address
+        u16_t      val;  // 16-bit register value
     } reg_u16_s;
 
-    /// @struct axe3d_s16_s
-    /// @brief  3D Axe 16-bit
+
+    /// @struct axe3d_i16_s
+    /// @brief  16-bit integer 3D axe
     typedef struct
     {
-        s16_t x;    // 16-bit x-axis
-        s16_t y;    // 16-bit y-axis
-        s16_t z;    // 16-bit z-axis
-    } axe3d_s16_s;
+        i16_t x;  // 16-bit integer x-axis
+        i16_t y;  // 16-bit integer y-axis
+        i16_t z;  // 16-bit integer z-axis
+    } axe3d_i16_s;
 
 #else
-    #warning "16-bit types are not supported."
+    #warning "16-bit integer types are not supported."
 #endif
 
 
-/* (32-bit) int */
+/* 32-bit Types                                                     */
+/* ---------------------------------------------------------------- */
 
 
 #if defined(__SIZEOF_INT__) && VALIDATE_TYPE(__SIZEOF_INT__, 4)
 
-    typedef unsigned int u32_t;
-    typedef u32_t  const u32c_t;
-    #define U32_T__MAX ((u32_t)0xFFFFFFFF)          //  4294967295
+    typedef signed  int i32_t;                     // 32-bit signed integer
+    typedef i32_t const i32c_t;                    // 32-bit signed integer constant
+    #define I32_T__MAX ((i32_t)0x7FFFFFFF)         // 32-bit signed integer maximum value:  2147483647
+    #define I32_T__MIN ((i32_t)(-I32_T__MAX - 1))  // 32-bit signed integer minimum value: -2147483648
 
-    typedef signed  int s32_t;
-    typedef s32_t const s32c_t;
-    #define S32_T__MAX ((s32_t)0x7FFFFFFF)          //  2147483647
-    #define S32_T__MIN ((s32_t)(-S32_T__MAX - 1))   // -2147483648
+    typedef unsigned int u32_t;                    // 32-bit unsigned integer
+    typedef u32_t  const u32c_t;                   // 32-bit unsigned integer constant
+    #define U32_T__MAX ((u32_t)0xFFFFFFFF)         // 32-bit maximum value: 4294967295
 
-    typedef u32_t         str32_t;
-    typedef str32_t const str32c_t;
-    typedef str32c_t    * str32l_t;
+
+    #ifndef char32_t
+    typedef u32_t          char32_t;   // 32-bit character
+    #endif
+    typedef char32_t const char32c_t;  // 32-bit character constant
+    typedef char32_t     * str32p_t;   // 32-bit character string pointer
+    typedef char32c_t    * str32r_t;   // 32-bit character string read-only pointer
+
 
     /// @union adr_u32_u
-    /// @brief Address 32-bit ( byte_0 | byte_1 | byte_2 | byte_3 )
+    /// @brief 32-bit address ( byte_0 | byte_1 | byte_2 | byte_3 )
     typedef union
     {
-        u32_t full;         // 32-bit register full address
+        u32_t full;   // 32-bit register full address
         struct {
-            u8_t byte_0;    // 32-bit register address byte 0
-            u8_t byte_1;    // 32-bit register address byte 1
-            u8_t byte_2;    // 32-bit register address byte 2
-            u8_t byte_3;    // 32-bit register address byte 3
-        };
+            u8_t _0;  // 32-bit register address byte 0
+            u8_t _1;  // 32-bit register address byte 1
+            u8_t _2;  // 32-bit register address byte 2
+            u8_t _3;  // 32-bit register address byte 3
+        } byte;       // 32-bit register address bytes
     } adr_u32_u;
 
+
     /// @union adr_u32c_u
-    /// @brief Address 32-bit ( byte_0 | byte_1 | byte_2 | byte_3 )
+    /// @brief 32-bit address ( byte_0 | byte_1 | byte_2 | byte_3 )
     typedef adr_u32_u const adr_u32c_u;
 
+
     /// @struct reg_u32_s
-    /// @brief  Register 32-bit ( byte_0 | byte_1 | byte_2 | byte_3 )
+    /// @brief  32-bit register ( byte_0 | byte_1 | byte_2 | byte_3 )
     typedef struct
     {
-        adr_u32c_u ADR;   // 32-bit register address
-        u32_t      val;   // 32-bit register value
+        adr_u32c_u ADR;  // 32-bit register address
+        u32_t      val;  // 32-bit register value
     } reg_u32_s;
 
-    /// @struct axe3d_s32_s
-    /// @brief  3D Axe 32-bit
+
+    /// @struct axe3d_i32_s
+    /// @brief  32-bit integer 3D axe
     typedef struct
     {
-        s32_t x;    // 32-bit x-axis
-        s32_t y;    // 32-bit y-axis
-        s32_t z;    // 32-bit z-axis
-    } axe3d_s32_s;
+        i32_t x;  // 32-bit integer x-axis
+        i32_t y;  // 32-bit integer y-axis
+        i32_t z;  // 32-bit integer z-axis
+    } axe3d_i32_s;
 
 #else
-    #warning "32-bit types are not supported."
+    #warning "32-bit integer types are not supported."
 #endif
-
-
-/* (64-bit) long long */
-
-
-#if defined(__SIZEOF_LONG_LONG__) && VALIDATE_TYPE(__SIZEOF_LONG_LONG__, 8)
-
-    typedef unsigned long long u64_t;
-    typedef u64_t        const u64c_t;
-    #define U64_T__MAX ((u64_t)0xFFFFFFFFFFFFFFFF)  //  18446744073709551615
-
-    typedef signed long long s64_t;
-    typedef s64_t      const s64c_t;
-    #define S64_T__MAX ((s64_t)0x7FFFFFFFFFFFFFFF)  //  9223372036854775807
-    #define S64_T__MIN ((s64_t)(-S64_T__MAX - 1))   // -9223372036854775808
-
-    /// @union adr_u64_u
-    /// @brief Address 64-bit ( byte_0 | byte_1 | byte_2 | byte_3 | byte_4 | byte_5 | byte_6 | byte_7 )
-    typedef union
-    {
-        u64_t full;         // 64-bit register full address
-        struct {
-            u8_t byte_0;    // 64-bit register address byte 0
-            u8_t byte_1;    // 64-bit register address byte 1
-            u8_t byte_2;    // 64-bit register address byte 2
-            u8_t byte_3;    // 64-bit register address byte 3
-            u8_t byte_4;    // 64-bit register address byte 4
-            u8_t byte_5;    // 64-bit register address byte 5
-            u8_t byte_6;    // 64-bit register address byte 6
-            u8_t byte_7;    // 64-bit register address byte 7
-        };
-    } adr_u64_u;
-
-    /// @union adr_u64c_u
-    /// @brief Address 64-bit ( byte_0 | byte_1 | byte_2 | byte_3 | byte_4 | byte_5 | byte_6 | byte_7 )
-    typedef adr_u64_u const adr_u64c_u;
-
-    /// @struct reg_u64_s
-    /// @brief  Register 64-bit ( byte_0 | byte_1 | byte_2 | byte_3 | byte_4 | byte_5 | byte_6 | byte_7 )
-    typedef struct
-    {
-        adr_u64c_u ADR;   // 64-bit register address
-        u64_t      val;   // 64-bit register value
-    } reg_u64_s;
-
-    /// @struct axe3d_s64_s
-    /// @brief  3D Axe 64-bit
-    typedef struct
-    {
-        s64_t x;    // 64-bit x-axis
-        s64_t y;    // 64-bit y-axis
-        s64_t z;    // 64-bit z-axis
-    } axe3d_s64_s;
-
-#else
-    #warning "64-bit types are not supported."
-#endif
-
-
-/* (32-bit) float */
 
 
 #if defined(__SIZEOF_FLOAT__) && VALIDATE_TYPE(__SIZEOF_FLOAT__, 4)
 
-    typedef float       f32_t;
-    typedef f32_t const f32c_t;
-    #define F32_T__MAX  3.402823466e+38F    //  Maximum float
-    #define F32_T__MIN -3.402823466e+38F    //  Minimum float
+    typedef float       f32_t;            // 32-bit float
+    typedef f32_t const f32c_t;           // 32-bit float constant
+    #define F32_T__MAX  3.402823466e+38F  // 32-bit float maximum value:  3.402823466e+38F
+    #define F32_T__MIN -3.402823466e+38F  // 32-bit float minimum value: -3.402823466e+38F
+
 
     /// @struct axe3d_f32_s
-    /// @brief  3D Axe 32-bit
+    /// @brief  32-bit float 3D axe
     typedef struct
     {
-        f32_t x;    // 32-bit x-axis (float)
-        f32_t y;    // 32-bit y-axis (float)
-        f32_t z;    // 32-bit z-axis (float)
+        f32_t x;  // 32-bit float x-axis
+        f32_t y;  // 32-bit float y-axis
+        f32_t z;  // 32-bit float z-axis
     } axe3d_f32_s;
 
 #else
-    #warning "32-bit float is not supported."
+    #warning "32-bit float types are not supported."
 #endif
 
 
-/* (64-bit) double */
+/* 64-bit Types                                                     */
+/* ---------------------------------------------------------------- */
+
+
+#if defined(__SIZEOF_LONG_LONG__) && VALIDATE_TYPE(__SIZEOF_LONG_LONG__, 8)
+
+    typedef signed long long i64_t;                 // 64-bit signed integer
+    typedef i64_t      const i64c_t;                // 64-bit signed integer constant
+    #define I64_T__MAX ((i64_t)0x7FFFFFFFFFFFFFFF)  // 64-bit signed integer maximum value:  9223372036854775807
+    #define I64_T__MIN ((i64_t)(-I64_T__MAX - 1))   // 64-bit signed integer minimum value: -9223372036854775808
+
+    typedef unsigned long long u64_t;               // 64-bit unsigned integer
+    typedef u64_t        const u64c_t;              // 64-bit unsigned integer constant
+    #define U64_T__MAX ((u64_t)0xFFFFFFFFFFFFFFFF)  // 64-bit unsigned integer maximum value: 18446744073709551615
+
+
+    /// @union adr_u64_u
+    /// @brief 64-bit address ( byte_0 | byte_1 | byte_2 | byte_3 | byte_4 | byte_5 | byte_6 | byte_7 )
+    typedef union
+    {
+        u64_t full;   // 64-bit register full address
+        struct {
+            u8_t _0;  // 64-bit register address byte 0
+            u8_t _1;  // 64-bit register address byte 1
+            u8_t _2;  // 64-bit register address byte 2
+            u8_t _3;  // 64-bit register address byte 3
+            u8_t _4;  // 64-bit register address byte 4
+            u8_t _5;  // 64-bit register address byte 5
+            u8_t _6;  // 64-bit register address byte 6
+            u8_t _7;  // 64-bit register address byte 7
+        } byte;       // 64-bit register address bytes
+    } adr_u64_u;
+
+
+    /// @union adr_u64c_u
+    /// @brief 64-bit address ( byte_0 | byte_1 | byte_2 | byte_3 | byte_4 | byte_5 | byte_6 | byte_7 )
+    typedef adr_u64_u const adr_u64c_u;
+
+
+    /// @struct reg_u64_s
+    /// @brief  64-bit register ( byte_0 | byte_1 | byte_2 | byte_3 | byte_4 | byte_5 | byte_6 | byte_7 )
+    typedef struct
+    {
+        adr_u64c_u ADR;  // 64-bit register address
+        u64_t      val;  // 64-bit register value
+    } reg_u64_s;
+
+
+    /// @struct axe3d_i64_s
+    /// @brief  64-bit integer 3D axe
+    typedef struct
+    {
+        i64_t x;  // 64-bit integer x-axis
+        i64_t y;  // 64-bit integer y-axis
+        i64_t z;  // 64-bit integer z-axis
+    } axe3d_i64_s;
+
+#else
+    #warning "64-bit integer types are not supported."
+#endif
 
 
 #if defined(__SIZEOF_DOUBLE__) && VALIDATE_TYPE(__SIZEOF_DOUBLE__, 8)
 
-    typedef double      f64_t;
-    typedef f64_t const f64c_t;
-    #define F64_T__MAX  1.7976931348623157e+308     //  Maximum double
-    #define F64_T__MIN -1.7976931348623157e+308     //  Minimum double
+    typedef double      f64_t;                   // 64-bit float
+    typedef f64_t const f64c_t;                  // 64-bit float constant
+    #define F64_T__MAX  1.7976931348623157e+308  // 64-bit float maximum value:  1.7976931348623157e+308
+    #define F64_T__MIN -1.7976931348623157e+308  // 64-bit float minimum value: -1.7976931348623157e+308
 
     /// @struct axe3d_f64_s
-    /// @brief  3D Axe 64-bit (float)
+    /// @brief  64-bit float 3D axe
     typedef struct
     {
-        f64_t x;    // 64-bit x-axis (float)
-        f64_t y;    // 64-bit y-axis (float)
-        f64_t z;    // 64-bit z-axis (float)
+        f64_t x;  // 64-bit float x-axis
+        f64_t y;  // 64-bit float y-axis
+        f64_t z;  // 64-bit float z-axis
     } axe3d_f64_s;
 
 #else
-    #warning "64-bit double is not supported."
+    #warning "64-bit float types are not supported."
 #endif
 
 
 /* (void) */
 
 
-typedef void       v0_t;
-typedef void const v0c_t;
+typedef void        v0_t;   // void type
+typedef v0_t  const v0c_t;  // void constant
+typedef v0_t      * v0p_t;  // void pointer           (generic pointer)
+typedef v0c_t     * v0r_t;  // void read-only pointer (opaque pointer)
 
 
 #endif /* __ISM330DHCX_TYPES_H__ */
